ab38faad31c43bebe2ff97b2553c636e30e7adb2
Nicolas Llancaqueo <n.llancaqueo01@ufromail.cl>
2024-11-09 20:30:12 -0300

    Delete: eliminación characters en backend

--- Backend/routes/charRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getCharacters } = require('../controller/charController');
-const router = express.Router();
-
-// Ruta para obtener personajes
-router.get('/characters', getCharacters);
-
-module.exports = router;
